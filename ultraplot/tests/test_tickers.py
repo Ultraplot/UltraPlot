95e0bc32c287d6e9933462a8ad881811e7121a4b
cvanelteren <caspervanelteren@gmail.com>
2025-09-10 12:32:29 +0200

    add more unittests

--- ultraplot/tests/test_tickers.py
+++ ultraplot/tests/test_tickers.py
@@ -40,3 +40,98 @@ def test_datetime_calendars_comparison():
     axs[1].plot(da_noleap)
 
     return fig
+
+
+@pytest.mark.parametrize(
+    "calendar",
+    [
+        "standard",
+        "gregorian",
+        "proleptic_gregorian",
+        "julian",
+        "all_leap",
+        "360_day",
+        "365_day",
+        "366_day",
+    ],
+)
+def test_datetime_calendars(calendar):
+    time = xr.date_range("2000-01-01", periods=365 * 10, calendar=calendar)
+    da = xr.DataArray(
+        data=np.sin(np.linspace(0, 2 * np.pi, 365 * 10)),
+        dims=("time",),
+        coords={"time": time},
+    )
+    fig, ax = uplt.subplots()
+    ax.plot(da)
+    ax.format(title=f"Calendar: {calendar}")
+
+
+@pytest.mark.mpl_image_compare
+def test_datetime_short_range():
+    time = xr.date_range("2000-01-01", periods=10, calendar="standard")
+    da = xr.DataArray(
+        data=np.sin(np.linspace(0, 2 * np.pi, 10)),
+        dims=("time",),
+        coords={"time": time},
+    )
+    fig, ax = uplt.subplots()
+    ax.plot(da)
+    ax.format(title="Short time range (days)")
+    return fig
+
+
+@pytest.mark.mpl_image_compare
+def test_datetime_long_range():
+    time = xr.date_range(
+        "2000-01-01", periods=365 * 200, calendar="standard"
+    )  # 200 years
+    da = xr.DataArray(
+        data=np.sin(np.linspace(0, 2 * np.pi, 365 * 200)),
+        dims=("time",),
+        coords={"time": time},
+    )
+    fig, ax = uplt.subplots()
+    ax.plot(da)
+    ax.format(title="Long time range (centuries)")
+    return fig
+
+
+def test_datetime_explicit_formatter():
+    time = xr.date_range("2000-01-01", periods=365 * 2, calendar="noleap")
+    da = xr.DataArray(
+        data=np.sin(np.linspace(0, 2 * np.pi, 365 * 2)),
+        dims=("time",),
+        coords={"time": time},
+    )
+    fig, ax = uplt.subplots()
+    ax.plot(da)
+    ax.format(
+        title="Explicit formatter",
+        xformatter=uplt.ticker.DatetimeFormatter("%b %Y", calendar="noleap"),
+    )
+    fig.canvas.draw()
+    labels = [label.get_text() for label in ax.get_xticklabels()]
+    assert len(labels) > 0
+    # check first label
+    try:
+        import cftime
+
+        cftime.datetime.strptime(labels[1], "%b %Y", calendar="noleap")
+    except (ValueError, IndexError):
+        assert False, f"Label {labels[1]} does not match format %b %Y"
+
+
+def test_datetime_maxticks():
+    time = xr.date_range("2000-01-01", periods=365 * 20, calendar="noleap")
+    da = xr.DataArray(
+        data=np.sin(np.linspace(0, 2 * np.pi, 365 * 20)),
+        dims=("time",),
+        coords={"time": time},
+    )
+    fig, ax = uplt.subplots()
+    ax.plot(da)
+    locator = ax.xaxis.get_major_locator()
+    locator.set_params(maxticks=5)
+    fig.canvas.draw()
+    assert len(ax.get_xticks()) <= 5
