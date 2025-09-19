d65c14132476ff1f8d15a043cf338eb85217997e
cvanelteren <caspervanelteren@gmail.com>
2025-09-19 21:12:43 +0200

    restore typo with sharing level

--- ultraplot/figure.py
+++ ultraplot/figure.py
@@ -1965,7 +1965,7 @@ class Figure(mfigure.Figure):
         # When we apply formatting to all axes, we need
         # to potentially adjust the labels.
 
-        if len(axs) == len(self.axes) and (self._sharex >= 3 and self._sharey >= 3):
+        if len(axs) == len(self.axes) and (self._sharex > 0 or self._sharey > 0):
             self._share_labels_with_others()
 
         # Warn unused keyword argument(s)
