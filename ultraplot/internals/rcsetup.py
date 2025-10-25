12a443b2f2cea4086305ff2efb528d6508ae945e
cvanelteren <caspervanelteren@gmail.com>
2025-10-25 11:15:11 +0200

    set default to false

--- ultraplot/internals/rcsetup.py
+++ ultraplot/internals/rcsetup.py
@@ -1946,7 +1946,7 @@ _rc_ultraplot_table = {
         "using ``format(units, unitformat)``. See also :rcraw:`autoformat`.",
     ),
     "ultraplot.check_for_latest_version": (
-        True,
+        False,
         _validate_bool,
         "Whether to check for the latest version of UltraPlot on PyPI when importing",
     ),
