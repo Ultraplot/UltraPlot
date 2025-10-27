25213616eb70c67063155e7b7795d3f64558d9cb
cvanelteren <caspervanelteren@gmail.com>
2025-10-27 21:52:59 +0100

    add copy array

--- ultraplot/demos.py
+++ ultraplot/demos.py
@@ -957,6 +957,7 @@ def show_colors(*, nhues=17, minsat=10, unknown="User", include=None, ignore=Non
             names = np.array([name for ipairs in hclpairs for name, _ in ipairs])
             ncols, nrows = 4, len(names) // 4 + 1
 
+        names = np.asarray(names).copy()
         names.resize((ncols, nrows))  # fill empty slots with empty string
         namess[cat] = names
 
