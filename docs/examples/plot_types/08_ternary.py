94d75afb72afc2365eb64ada69462bd7bcc663ea
cvanelteren <caspervanelteren@gmail.com>
2026-01-27 19:29:09 +1000

    update workflow

--- docs/examples/plot_types/08_ternary.py
+++ docs/examples/plot_types/08_ternary.py
@@ -22,14 +22,13 @@ t, l, r, v = get_shanon_entropies()
 
 fig, ax = uplt.subplots(projection = "ternary")
 vmin = 0.0
-vmax = 1.2
+vmax = 1.0
 levels = np.linspace(vmin, vmax, 7)
 
 cs = ax.tripcolor(t, l, r, v, cmap = "lapaz_r", shading='flat', vmin=vmin, vmax=vmax)
 ax.set_title("Ternary Plot of Shannon Entropies")
 ax.plot(*get_spiral(), color='white', lw=1.25)
-colorbar = ax.colorbar(cs)
-colorbar.set_label('Entropy', rotation=270, va='baseline')
+colorbar = ax.colorbar(cs, loc = "b", align = 'c', title = "Entropy", length = 0.33,)
 
 fig.show()
 uplt.show(block = 1)
