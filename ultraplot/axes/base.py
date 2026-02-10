ba36416a46d9876ef9277f524e43a3e4a6337961
cvanelteren <caspervanelteren@gmail.com>
2026-02-10 11:25:59 +1000

    black

--- ultraplot/axes/base.py
+++ ultraplot/axes/base.py
@@ -4040,7 +4040,9 @@ class Axes(_ExternalModeMixin, maxes.Axes):
             Iterable[float],
             np.ndarray,
         ],
-        xytext: Optional[Union[Tuple[float, float], Iterable[float], np.ndarray]] = None,
+        xytext: Optional[
+            Union[Tuple[float, float], Iterable[float], np.ndarray]
+        ] = None,
         xycoords: Union[str, mtransforms.Transform] = "data",
         textcoords: Optional[Union[str, mtransforms.Transform]] = None,
         arrowprops: Optional[dict[str, Any]] = None,
