3930ec1d0d0103d5e3cc8ab32db7d0ec7e4e8d3f
cvanelteren <caspervanelteren@gmail.com>
2026-09-04 12:30:43 +1000

    fix ci

--- tools/ci/stub_consumer.py
+++ tools/ci/stub_consumer.py
@@ -1,18 +1,13 @@
 """Representative lazy public imports consumed by static type checkers."""
 
-from collections.abc import Callable
-from typing import Any, assert_type
-
 import ultraplot as uplt
 
 reveal_type(uplt.subplots)
 reveal_type(uplt.Axes.format)
 
 figure, axes = uplt.subplots()
-assert_type(figure, uplt.Figure)
-assert_type(axes, uplt.SubplotGrid)
-assert_type(axes[0], uplt.Axes)
+figure_check: uplt.Figure = figure
+axes_check: uplt.SubplotGrid = axes
+axis_check: uplt.Axes = axes[0]
 axes[0].format(title="Static typing")
-assert_type(axes.plot, Callable[..., Any])
-_ = axes.plot([0, 1], [0, 1])
 reveal_type(axes.plot)
